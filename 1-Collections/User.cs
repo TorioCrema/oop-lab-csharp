c118a156e8709e031d147f15c0ec0c9353d7cb40
Simone Zama <simone.zama@studio.unibo.it>
2021-12-14 15:20:38 +0100

    Implement User constructor

--- 1-Collections/User.cs
+++ 1-Collections/User.cs
@@ -6,18 +6,22 @@ namespace Collections
     {
         public User(string fullName, string username, uint? age)
         {
-            throw new NotImplementedException("TODO ensure that username is not null");
-            throw new NotImplementedException("TODO initialise this instance of user accordingly");
+            FullName = fullName;
+            Username = username ?? throw new ArgumentNullException();
+            if (age.HasValue)
+            {
+                Age = age;
+            }
         }
-        
+
         public uint? Age { get; }
-        
+
         public string FullName { get; }
-        
+
         public string Username { get; }
 
         public bool IsAgeDefined => throw new NotImplementedException("TODO check whether age is non-null or not");
-        
+
         // TODO implement missing methods (try to autonomously figure out which are the necessary methods)
     }
 }
