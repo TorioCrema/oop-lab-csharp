06b0f773ae9ee5a1f3b3ee81617a0ef7aaf27197
Simone Zama <simone.zama@studio.unibo.it>
2021-12-15 09:20:07 +0100

    Add Exception to GetFirst method

--- 2-Exceptions/FixedSizeQueue.cs
+++ 2-Exceptions/FixedSizeQueue.cs
@@ -6,13 +6,13 @@ namespace Exceptions
         private object[] _items;
         private int _firstIndex = 0;
         private int _lastIndex = 0;
-        
+
         public FixedSizeQueue(uint capacity)
         {
             Capacity = capacity;
             _items = new object[capacity];
         }
-        
+
         public uint Capacity { get; }
 
         public uint Count => (uint) (_lastIndex - _firstIndex);
@@ -20,11 +20,15 @@ namespace Exceptions
         public object GetFirst()
         {
             // TODO ensure objects can only be retrieve if the item is queue is not empty
+            if (Count == 0)
+            {
+                throw new EmptyQueueException("Cannot retrieve item from empty queue.");
+            }
             var first = _items[_firstIndex % Capacity];
             _firstIndex++;
             return first;
         }
-        
+
         public void AddLast(object item)
         {
             // TODO ensure objects can only be inserted if the item is queue is not full
